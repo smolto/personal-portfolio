c17ac112a0025871a9037afa8c85f4d98184f122
Santi Molto <sant8613@gmail.com>
2021-05-12 18:45:46 +0200

    Home page casi terminada al completo

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,43 +1,61 @@
+import { useState } from 'react'
+
 import 'src/components/ContactForm/ContactForm.css'
 
 import Input from 'src/components/Input/Input'
 
 const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [reason, setReason] = useState('');
+
   return (
     <div className="contact-wrapper">
       <div className="contact-wrapper__title">
         <h1>Contact</h1>
       </div>
-      <form>
+      <form target="_blank" action="https://formsubmit.co/santiagomoltomartinez@gmail.com	" method="POST">
         <div className="contact-wrapper__grid">
           <Input
             type={`text`}
-            value={``}
+            name={`name`}
+            value={name}
+            onChange={setName}
             placeholder={`Name ...`}
             cssClass={`input medium-text`}
           />
           <Input
             type={`text`}
-            value={``}
+            name={`surname`}
+            value={surname}
+            onChange={setSurname}
             placeholder={`Surname ...`}
             cssClass={`input medium-text`}
           />
           <Input
             type={`email`}
-            value={``}
+            name={`email`}
+            value={email}
+            onChange={setEmail}
             placeholder={`Email ...`}
             cssClass={`input medium-text`}
           />
           <Input
             type={`number`}
-            value={``}
+            name={`phone`}
+            value={phone}
+            onChange={setPhone}
             placeholder={`Phone number ...`}
             cssClass={`input medium-text`}
           />
           <div className="col-2">
             <Input
               type={`textarea`}
-              value={``}
+              name={`reason`}
+              value={reason}
+              onChange={setReason}
               placeholder={`Reason ...`}
               cssClass={`input medium-text`}
             />
@@ -49,6 +67,12 @@ const ContactForm = () => {
             value={`Send ✉️`}
             cssClass={`btn tertiary large-text`}
           />
+        </div>
+        <div className="formsubmit_info">
+          <span>Powered by</span>
+          <a target="_blank" href="https://formsubmit.co/?utm_source=formsubmit.co&amp;utm_medium=site%20link&amp;utm_campaign=submission%20page">
+            <img height="35px" src="https://formsubmit.co/image/logo.png" alt="FormSubmit logo" />
+          </a>
         </div>
       </form>
     </div>
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,23 +1,26 @@
 import 'src/components/Input/Input.css'
 
-const Input = ({ type, value, placeholder, cssClass, name }) => {
+const Input = ({ type, value, placeholder, cssClass, name, onChange }) => {
 
   if (type !== 'textarea')
     return (
       <input
         className={`${cssClass}`}
+        name={name}
         type={type}
         value={value}
         placeholder={placeholder}
-        name={name}
+        onChange={(e) => onChange(e.target.value)}
       />
     )
   else
     return (
       <textarea
-        name={name}
         className={`textarea ${cssClass}`}
+        name={name}
+        value={value}
         placeholder={placeholder}
+        onChange={(e) => onChange(e.target.value)}
         cols="20"
         rows="7" 
       />
